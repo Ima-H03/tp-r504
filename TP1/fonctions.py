4bb9b69f3ec766fdb81d33f265ee585315c48f21
vorax12 <imabith.houngbo@univ-rouen.fr>
2026-09-16 14:39:52 +0200

    Ajout des tests des cas limites

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,6 @@
 def puiss(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+    if a == 0 and b < 0:
+        raise ValueError("Une puissance négative de 0 est indéfinie")
     return a**b
--- TP1/test.py
+++ TP1/test.py
@@ -1,9 +1,11 @@
 import pytest
 import fonctions as f
 
+
 def test_1():
     assert f.puiss(2,3) == 8
     assert f.puiss(2,2) == 4
+
 
 def test_2():
     assert f.puiss(-1,2) == 1
@@ -12,3 +14,15 @@ def test_2():
     assert f.puiss(-1,-2) == 1
     assert f.puiss(-2, -1) == -0.5
 
+
+
+def test_3():
+    assert f.puiss(0,1) == 0
+    assert f.puiss(0,2) == 0
+    assert f.puiss(0,10) == 0
+    
+    with pytest.raises(ValueError):
+        f.puiss(0,-1)
+
+    with pytest.raises(ValueError):
+        f.puiss(0, -2)    
