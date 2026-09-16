d7196f36d7ff19a9b126f15376e4e4e50f595949
vorax12 <imabith.houngbo@univ-rouen.fr>
2026-09-16 15:10:52 +0200

    Remplacement de la puissance par une boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,17 @@
 def puiss(a, b):
+
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+
     if a == 0 and b < 0:
         raise ValueError("Une puissance négative de 0 est indéfinie")
-    return a**b
+
+    resultat = 1
+
+    for i in range(abs(b)):
+        resultat = resultat * a
+
+    if b < 0:
+        resultat = 1 / resultat
+
+    return resultat
