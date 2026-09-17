5ac3d797dbbbdd0c7952f68246fb214f014c17d3
vorax12 <imabith.houngbo@univ-rouen.fr>
2026-09-17 15:32:02 +0200

    Ajout des clients et serveurs TCP UDP

---
+++ tp-java/ClientUDP.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientUDP
+{
+	public static void main ( String [ ] args )
+	{
+		try
+		{
+			InetAddress addr = InetAddress.getLocalHost();
+			System.out.println( "adresse=" + addr.getHostName() );
+
+			String s = "Hello World";
+			byte[] data = s.getBytes();
+
+			DatagramPacket packet = new DatagramPacket( data, data.length, addr
+				, 1234 );
+
+			DatagramSocket sock = new DatagramSocket();
+			sock.send(packet);
+
+			DatagramPacket response = new DatagramPacket(
+				new byte[1024],
+				1024
+			);
+
+			sock.receive(response);
+
+			String str = new String(response.getData());
+
+			System.out.println( "réponse=" + str );
+
+			sock.close();
+		}
+		catch ( Exception ex )
+		{
+			System.out.println( "erreur !" );
+			ex.printStackTrace();
+		}
+	}
+}
