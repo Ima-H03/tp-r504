caa2640cb24f513bc4efc1ac69ceeb168ceb050c
vorax12 <imabith.houngbo@univ-rouen.fr>
2026-09-17 15:37:49 +0200

    fin

--- tp-java/ClientTCP2.java
+++ tp-java/ClientTCP2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class ClientTCP1
+public class ClientTCP2
 {
 	public static void main ( String [ ] args )
 	{
@@ -13,7 +13,7 @@ public class ClientTCP1
 				socket.getOutputStream()
 			);
 
-			dOut.writeUTF( "message test" );
+			dOut.writeUTF( args[0] );
 
 			socket.close();
 		}
